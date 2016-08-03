29297d8203ff7bb4ce6ed2c0e624416bb2e461a1
AdManteza <vagrant@lighthouse-labs>
2016-08-03 19:21:07 +0000

    Completed exercise4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -8,9 +8,6 @@ puts "----------"
 
 # Your code goes here ...
 
-# * Surrey (annual_revenue of 224000, carries women's apparel only)
-#   * Whistler (annual_revenue of 1900000 carries men's apparel only)
-#   * Yaletown (annual_revenue of 430000 carries men's and women's apparel)
 Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
 Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
@@ -21,8 +18,9 @@ Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, women
   puts "#{store.name} has a annual revenue of:#{store.annual_revenue}"
 end
 
-@womens_stores = Store.where(womens_apparel: true)
+@womens_stores = Store.where("annual_revenue >= ? AND womens_apparel = ?", 1000000, true)
 
 @womens_stores.each do |store|
   puts "#{store.name} has a annual revenue of:#{store.annual_revenue}"
 end
+
