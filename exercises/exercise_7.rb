503598fbb9368ba17144d3ece0b0ae52430e234c
AdManteza <vagrant@lighthouse-labs>
2016-08-03 21:01:24 +0000

    completed exercise7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# will not create a new employee
+@store1.employees.create(first_name: "Ginger", last_name: "Guo", hourly_rate: 300)
+@store1.employees.create(hourly_rate: 100)
+
+# will create a new employee
+@store1.employees.create(first_name: "Shaun", last_name: "Lloyd", hourly_rate: 50)
+
+# will not create a new store
+@store10 = Store.create(name: "Penshoppe", annual_revenue: 224000, mens_apparel: false, womens_apparel: false)
+puts "#{@store10.errors.messages}"
+
+puts "Enter a new store name"
+store_name = gets.chomp
+
+@new_store = Store.create(name: store_name)
+
+puts "#{@new_store.errors.messages}"
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, :annual_revenue, presence: true 
+  validates :mens_apparel, :womens_apparel, :inclusion => { :in => [true, false] }
+  validate :stores_must_have_mens_or_womens_apparel, on: :create
+
+  def stores_must_have_mens_or_womens_apparel
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "Must have a men's apparel")
+      errors.add(:womens_apparel, "Must have a women's apparel")
+    end
+  end
+ 
 end
+
